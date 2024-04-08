5c668862fb20802ee0de5293b430a873b3eaa119
algot <algot.lindblom@gmail.com>
2024-04-08 10:29:59 +0200

    initial commit

---
+++ simplepaint/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace simplepaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rityta = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pBoxColorDialog = new System.Windows.Forms.PictureBox();
+            this.widthSelectionBar = new System.Windows.Forms.TrackBar();
+            this.widthSelectorGroup = new System.Windows.Forms.GroupBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog3 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.rityta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxColorDialog)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.widthSelectionBar)).BeginInit();
+            this.widthSelectorGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rityta
+            // 
+            this.rityta.Location = new System.Drawing.Point(12, 117);
+            this.rityta.Name = "rityta";
+            this.rityta.Size = new System.Drawing.Size(567, 435);
+            this.rityta.TabIndex = 0;
+            this.rityta.TabStop = false;
+            this.rityta.Paint += new System.Windows.Forms.PaintEventHandler(this.rityta_Paint);
+            this.rityta.MouseDown += new System.Windows.Forms.MouseEventHandler(this.rityta_MouseDown);
+            this.rityta.MouseMove += new System.Windows.Forms.MouseEventHandler(this.rityta_MouseMove);
+            this.rityta.MouseUp += new System.Windows.Forms.MouseEventHandler(this.rityta_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::simplepaint.Properties.Resources.cooltext454210699375667;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(567, 108);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pBoxColorDialog);
+            this.groupBox1.Location = new System.Drawing.Point(627, 181);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(98, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Färgval";
+            // 
+            // pBoxColorDialog
+            // 
+            this.pBoxColorDialog.BackColor = System.Drawing.Color.Black;
+            this.pBoxColorDialog.Location = new System.Drawing.Point(22, 29);
+            this.pBoxColorDialog.Name = "pBoxColorDialog";
+            this.pBoxColorDialog.Size = new System.Drawing.Size(51, 48);
+            this.pBoxColorDialog.TabIndex = 5;
+            this.pBoxColorDialog.TabStop = false;
+            this.pBoxColorDialog.Click += new System.EventHandler(this.pBoxColorDialog_Click);
+            // 
+            // widthSelectionBar
+            // 
+            this.widthSelectionBar.Location = new System.Drawing.Point(10, 19);
+            this.widthSelectionBar.Maximum = 30;
+            this.widthSelectionBar.Name = "widthSelectionBar";
+            this.widthSelectionBar.Size = new System.Drawing.Size(157, 45);
+            this.widthSelectionBar.TabIndex = 3;
+            this.widthSelectionBar.ValueChanged += new System.EventHandler(this.widthSelectionBar_ValueChanged);
+            // 
+            // widthSelectorGroup
+            // 
+            this.widthSelectorGroup.Controls.Add(this.widthSelectionBar);
+            this.widthSelectorGroup.Location = new System.Drawing.Point(585, 97);
+            this.widthSelectorGroup.Name = "widthSelectorGroup";
+            this.widthSelectorGroup.Size = new System.Drawing.Size(179, 78);
+            this.widthSelectorGroup.TabIndex = 4;
+            this.widthSelectorGroup.TabStop = false;
+            this.widthSelectorGroup.Text = "Tjocklek";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 575);
+            this.Controls.Add(this.widthSelectorGroup);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rityta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rityta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxColorDialog)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.widthSelectionBar)).EndInit();
+            this.widthSelectorGroup.ResumeLayout(false);
+            this.widthSelectorGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox rityta;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TrackBar widthSelectionBar;
+        private System.Windows.Forms.GroupBox widthSelectorGroup;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pBoxColorDialog;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.ColorDialog colorDialog3;
+    }
+}
+
---
+++ simplepaint/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simplepaint
+{
+    public partial class Form1 : Form
+    {
+
+        public Composer c = new Composer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            c.InitializeDrawingSurface();
+            widthSelectionBar.Value = 4;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rityta_MouseDown(object sender, MouseEventArgs e)
+        {
+            c.Down(e);
+        }
+
+        private void rityta_MouseMove(object sender, MouseEventArgs e)
+        {
+            c.Draw(rityta, e);
+        }
+
+        private void rityta_MouseUp(object sender, MouseEventArgs e)
+        {
+            c.Up();         // Användaren har slutat rita och släppt musknappen
+        }
+
+        private void rityta_Paint(object sender, PaintEventArgs e)
+        {
+            c.PaintEvent(e);    //Egentligen rätt onödig
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            c.InitializeDrawingSurface();
+            rityta.Invalidate();    //Återställ rityta vid klick på logo
+        }
+        
+
+        private void widthSelectionBar_ValueChanged(object sender, EventArgs e)
+        {
+            c.SetWidth(widthSelectionBar.Value);    // Enkel väljare för tjpocklek
+        }
+
+        private void pBoxColorDialog_Click(object sender, EventArgs e)
+        {
+            c.SetColor(pBoxColorDialog);
+        }
+    }
+    public class Composer
+    {
+        private bool isDrawing = false;     // En flagga som indikerar om användaren är i färd med att rita eller inte.
+        private Point previousPoint;        // Håller koll på den tidigare muspositionen för att rita linjer.
+        // private Color colorChoice;   Onödig med ColorDialog
+        private float width;
+        private ColorDialog colorPicker = new ColorDialog();
+
+        // Deklarera en bitmap för att lagra ritområdet
+        private Bitmap drawingSurface = new Bitmap(800, 600);
+
+        public Composer()
+        {
+            colorPicker.Color = Color.Black;
+            width = 4;
+        }
+        public void InitializeDrawingSurface()
+        {
+            using (Graphics g = Graphics.FromImage(drawingSurface))
+            {
+                g.Clear(Color.White);
+            }
+        }
+        public void Up()
+        {
+            isDrawing = false;
+        }
+        public void Down(MouseEventArgs e)
+        {
+            isDrawing = true;                   // Användaren har börjat rita                
+            previousPoint = e.Location;         // Sparar positionen där muspekaren befann sig när ritningen påbörjades i previousPoint
+        }
+        public void Draw(PictureBox rityta, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = Graphics.FromImage(drawingSurface))
+                {
+                    // Skapa en penna med svart färg och tjocklek 4
+                    Pen pen = new Pen(colorPicker.Color, width);
+
+                    // Rita en linje från föregående musposition till nuvarande musposition med den svarta pennan
+                    g.DrawLine(pen, previousPoint, e.Location);
+                    g.DrawRectangle(pen, new Rectangle(e.Location.X, e.Location.Y, (int)width, (int)width));
+                }
+                previousPoint = e.Location;
+
+                // Uppdatera PictureBox för att visa de ändringar som gjorts på ritområdet
+                rityta.Invalidate();
+            }
+        }
+        public void PaintEvent(PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(drawingSurface, Point.Empty);  // Rita till ritytan när PaintEvent kallas
+        }
+        public void SetColor(PictureBox item) 
+        {
+            if(colorPicker.ShowDialog() == DialogResult.OK)
+            {
+                item.BackColor = colorPicker.Color;
+            }
+        }
+        public void SetWidth(float val)
+        {
+            width = val;
+        }
+    }
+}
